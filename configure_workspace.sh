ed7e52849823f970f80b6d41fb7620634507263e
medecigo3 <arturo.medecigo@tecxis.com>
2025-08-30 14:16:50 +0200

    Update script to make various configuration scripts executable

--- configure_workspace.sh
+++ configure_workspace.sh
@@ -16,6 +16,30 @@ ln -sf ~/Dropbox/devArturo/repos/dev/dotfiles/aerospace/* ~/.config/aerospace
 ln -sf ~/Dropbox/devArturo/repos/dev/dotfiles/catppuccin/* ~/.config/catppuccin/
 ln -sf ~/Dropbox/devArturo/repos/dev/dotfiles/sketchybar/* ~/.config/sketchybar/
 ln -sf ~/Dropbox/devArturo/repos/dev/dotfiles/borders/*  ~/.config/borders
+
+#Grant executable rights
+chmod +x aerospace/organize-workspaces.sh
+chmod +x configure_workspace.sh
+chmod +x powerlevel10k/gitstatus/gitstatus.plugin.sh
+chmod +x powerlevel10k/gitstatus/gitstatus.prompt.sh
+chmod +x sketchybar/items/front_app.sh
+chmod +x sketchybar/plugins/aerospace.sh
+chmod +x sketchybar/plugins/battery.sh
+chmod +x sketchybar/plugins/clock.sh
+chmod +x sketchybar/plugins/front_app.sh
+chmod +x sketchybar/plugins/icon_map_fn.sh
+chmod +x sketchybar/plugins/load_spaces.sh
+chmod +x sketchybar/plugins/neutonfoo_battery.sh
+chmod +x sketchybar/plugins/neutonfoo_clock.sh
+chmod +x sketchybar/plugins/neutonfoo_volume.sh
+chmod +x sketchybar/plugins/reinit_workspaces.sh
+chmod +x sketchybar/plugins/space.sh
+chmod +x sketchybar/plugins/space_windows.sh
+chmod +x sketchybar/plugins/spotify.sh
+chmod +x sketchybar/plugins/update_workspace_icons.sh
+chmod +x sketchybar/plugins/volume.sh
+chmod +x sketchybar/plugins/weather.sh
+chmod +x sketchybar/plugins/workspace_monitor.sh
 #Source Zsh terminal
 source ~/.zshrc
 
